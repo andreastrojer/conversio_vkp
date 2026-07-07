47e30eef993edf83e1a5685eb900d14971047bf3
trojera <andreas.trojer@edu.htl-villach.at>
2026-07-07 14:34:17 +0200

    Microsoft Login erste Schritte

--- web/src/lib/auth.ts
+++ web/src/lib/auth.ts
@@ -1,12 +1,20 @@
-import type {NextAuthOptions} from 'next-auth'
+import NextAuth from 'next-auth'
+import MicrosoftEntraID from 'next-auth/providers/microsoft-entra-id'
 
-export const authOptions: NextAuthOptions = {
+export const {handlers, auth, signIn, signOut} = NextAuth({
   secret: process.env.AUTH_SECRET,
+  trustHost: true,
   session: {
     strategy: 'jwt',
   },
   pages: {
     signIn: '/login',
   },
-  providers: [],
-}
+  providers: [
+    MicrosoftEntraID({
+      clientId: process.env.AUTH_MICROSOFT_ENTRA_ID_ID,
+      clientSecret: process.env.AUTH_MICROSOFT_ENTRA_ID_SECRET,
+      issuer: process.env.AUTH_MICROSOFT_ENTRA_ID_ISSUER,
+    }),
+  ],
+})
